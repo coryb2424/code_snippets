6a666ccfc60b04b0b74e8dc94401107499156fe3
cory bonneau <cory.bonneau2424@gmail.com>
2018-07-18 10:32:46 +0200

    re-ordered how the data is displayed; title first

--- retrieve_imdb_movie_info/lib/scraper.rb
+++ retrieve_imdb_movie_info/lib/scraper.rb
@@ -16,17 +16,16 @@ def scrape_movie(url)
   html_doc = Nokogiri::HTML(html_file)
 
   movie_info = {}
-
-  movie_info[:director] = html_doc.at('//span[@itemprop = \'director\']').children.text.strip
-  movie_info[:cast] = html_doc.search('//span[@itemprop = \'actors\']').map do |element|
-    element.children.text.strip.delete(',')
-  end
-  movie_info[:storyline] = html_doc.at('.summary_text').children.text.strip
   movie_info[:title] = if html_doc.at('.originalTitle')
                          html_doc.at('.originalTitle').children[0].text
                        else
                          html_doc.at('.title_wrapper h1').children[0].text.strip
                        end
+  movie_info[:storyline] = html_doc.at('.summary_text').children.text.strip
+  movie_info[:director] = html_doc.at('//span[@itemprop = \'director\']').children.text.strip
+  movie_info[:cast] = html_doc.search('//span[@itemprop = \'actors\']').map do |element|
+    element.children.text.strip.delete(',')
+  end
   movie_info[:year] = html_doc.at('#titleYear').text.strip.delete('(').delete(')')
   movie_info
 end

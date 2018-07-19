84c284f940a3f6dd70146cc21f26674e10d2df00
cory bonneau <cory.bonneau2424@gmail.com>
2018-07-20 00:16:31 +0200

    created :credits, add relavent rspec

--- slot_machine/lib/slot_machine.rb
+++ slot_machine/lib/slot_machine.rb
@@ -1,29 +1,51 @@
 # Simplified SlotMachine with 3 identitcal or 2+joker rules.
 class SlotMachine
+  attr_reader :credits
+
+  PLAY_COST = 2
   SYMBOLS = %w[joker star bell seven cherry].freeze
   SYMBOL_MULTIPLIER = {
-    'joker' => 5,
-    'star' => 4,
-    'bell' => 3,
-    'seven' => 2,
+    'joker'  => 5,
+    'star'   => 4,
+    'bell'   => 3,
+    'seven'  => 2,
     'cherry' => 1
   }.freeze
 
+  def initialize(credits)
+    @credits = credits
+  end
+
   def pull_lever
+    return "Not enough credits - #{@credits} remaining" if @credits < 2
+    @credits -= PLAY_COST
     reels = generate_reels
     score = calculate_score(reels)
-    puts display(reels, score)
+    display(reels, score)
   end
+
+  private
 
   def calculate_score(reels)
     dup = reels.detect { |reel| reels.count(reel) > 1 }
     return 0 if dup.nil?
     dup_count = reels.count(dup)
     return 0 if dup_count == 2 && !reels.include?('joker')
-    dup_count == 3 ? multiplier(dup, 2) : multiplier(dup)
+    @credits += if dup_count == 3
+                  score = multiplier(dup, 2)
+                else
+                  score = multiplier(dup)
+                end
+    score
   end
 
-  private
+  def display(reels, score)
+    string = '|'
+    reels.each do |reel|
+      string << "#{reel}|"
+    end
+    string << "\nScore: #{score}"
+  end
 
   def generate_reels
     Array.new(3) do
@@ -33,13 +55,5 @@ class SlotMachine
 
   def multiplier(symbol, modifier = 1)
     SYMBOL_MULTIPLIER[symbol] * 5 * modifier
-  end
-
-  def display(reels, score)
-    string = '|'
-    reels.each do |reel|
-      string << "#{reel}|"
-    end
-    string << "\nScore: #{score}"
   end
 end
--- slot_machine/spec/slot_machine_spec.rb
+++ slot_machine/spec/slot_machine_spec.rb
@@ -1,50 +1,51 @@
 require_relative '../lib/slot_machine'
 
-def test_scenario(reels, expected_score)
+def test_scenario_reels(reels, expected_score)
   it "returns #{expected_score} for #{reels[0]}/#{reels[1]}/#{reels[2]}" do
-    expect(slot_machine.calculate_score(reels)).to eq expected_score
+    expect(slot_machine.send(:calculate_score, reels)).to eq expected_score
   end
 end
 
 describe SlotMachine do
-  let(:slot_machine) { SlotMachine.new }
+  let(:slot_machine) { SlotMachine.new(100) }
+
   describe '#calculate_score' do
     describe '- zero' do
-      test_scenario(%w[joker star cherry], 0)
-      test_scenario(%w[star star bell], 0)
-      test_scenario(%w[star star seven], 0)
-      test_scenario(%w[star star cherry], 0)
-      test_scenario(%w[bell bell star], 0)
-      test_scenario(%w[bell bell seven], 0)
-      test_scenario(%w[bell bell cherry], 0)
-      test_scenario(%w[seven seven star], 0)
-      test_scenario(%w[seven seven bell], 0)
-      test_scenario(%w[seven seven cherry], 0)
-      test_scenario(%w[cherry cherry star], 0)
-      test_scenario(%w[cherry cherry bell], 0)
-      test_scenario(%w[cherry cherry seven], 0)
+      test_scenario_reels(%w[joker star cherry], 0)
+      test_scenario_reels(%w[star star bell], 0)
+      test_scenario_reels(%w[star star seven], 0)
+      test_scenario_reels(%w[star star cherry], 0)
+      test_scenario_reels(%w[bell bell star], 0)
+      test_scenario_reels(%w[bell bell seven], 0)
+      test_scenario_reels(%w[bell bell cherry], 0)
+      test_scenario_reels(%w[seven seven star], 0)
+      test_scenario_reels(%w[seven seven bell], 0)
+      test_scenario_reels(%w[seven seven cherry], 0)
+      test_scenario_reels(%w[cherry cherry star], 0)
+      test_scenario_reels(%w[cherry cherry bell], 0)
+      test_scenario_reels(%w[cherry cherry seven], 0)
     end
 
     describe '- one joker' do
-      test_scenario(%w[star star joker], 20)
-      test_scenario(%w[bell bell joker], 15)
-      test_scenario(%w[seven seven joker], 10)
-      test_scenario(%w[cherry cherry joker], 5)
+      test_scenario_reels(%w[star star joker], 20)
+      test_scenario_reels(%w[bell bell joker], 15)
+      test_scenario_reels(%w[seven seven joker], 10)
+      test_scenario_reels(%w[cherry cherry joker], 5)
     end
 
     describe '- two jokers' do
-      test_scenario(%w[joker joker star], 25)
-      test_scenario(%w[joker joker bell], 25)
-      test_scenario(%w[joker joker seven], 25)
-      test_scenario(%w[joker joker cherry], 25)
+      test_scenario_reels(%w[joker joker star], 25)
+      test_scenario_reels(%w[joker joker bell], 25)
+      test_scenario_reels(%w[joker joker seven], 25)
+      test_scenario_reels(%w[joker joker cherry], 25)
     end
 
     describe '- three of the same' do
-      test_scenario(%w[joker joker joker], 50)
-      test_scenario(%w[star star star], 40)
-      test_scenario(%w[bell bell bell], 30)
-      test_scenario(%w[seven seven seven], 20)
-      test_scenario(%w[cherry cherry cherry], 10)
+      test_scenario_reels(%w[joker joker joker], 50)
+      test_scenario_reels(%w[star star star], 40)
+      test_scenario_reels(%w[bell bell bell], 30)
+      test_scenario_reels(%w[seven seven seven], 20)
+      test_scenario_reels(%w[cherry cherry cherry], 10)
     end
   end
 
@@ -54,6 +55,19 @@ describe SlotMachine do
       reels2 = slot_machine.send(:generate_reels)
 
       expect(reels1).to_not eq reels2
+    end
+  end
+
+  describe '#credits' do
+    let(:slot_machine) { SlotMachine.new(2) }
+
+    it 'should increase/decrease' do
+      expect(slot_machine.pull_lever).to_not eq 2
+    end
+
+    let(:empty_slot_machine) { SlotMachine.new(0) }
+    it 'returns message if there aren\'t enough credits' do
+      expect(empty_slot_machine.pull_lever).to eq 'Not enough credits - 0 remaining'
     end
   end
 end
